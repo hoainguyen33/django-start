3711878317dc99f81e7562d3e0c059628637d7d4
hoainguyen33 <nguyenvanhoai.ks@gmail.com>
2022-04-24 21:19:05 +0700

    update static file

--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,9 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILE_DIRS = [
-#     os.path.join()
-# ]
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
