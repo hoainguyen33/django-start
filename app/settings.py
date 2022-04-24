b1aa2574d74b08be40e4be4b9e6902d4d104c120
hoainguyen33 <nguyenvanhoai.ks@gmail.com>
2022-04-24 21:11:06 +0700

    update templates

--- app/settings.py
+++ app/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# STATICFILE_DIRS = [
+#     os.path.join()
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
